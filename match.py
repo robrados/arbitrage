686419af63768404e4e00ef804fa714f3471adcb
MLoydd <mike.loydd@outlook.com>
2018-06-15 17:05:11 +0200

    fixed json decoding issue

--- match.py
+++ match.py
@@ -1,15 +1,11 @@
-
 class Match:
 
-    def __init__(self, player1, player2, quote1 = "", quote2 = "", league = ""):
+    def __init__(self, player1, player2, quote1="", quote2="", league=""):
         self.player1 = player1
         self.player2 = player2
         self.quote1 = quote1
         self.quote2 = quote2
         self.league = league
 
-
     def __str__(self):
-        return "player 1 : %s , player 2 : %s" %  (self.player1, self.player2)
-
-
+        return "player 1 : %s , player 2 : %s" % (self.player1, self.player2)
--- source.py
+++ source.py
@@ -1,14 +1,15 @@
 import json
 
+
 def get_sources():
     with open('./configs/egb_config.json', encoding='utf-8') as file:
         data = json.loads(file.read(), object_hook=__as_sources)
 
 
-def __as_sources(dict):
-    list = []
-    for s in dict:
-        list.append(Source(s['url'], s['home'], s['guest'], s['homeQuote'], s['guestQuote']))
+def __as_sources(dct):
+    if 'url' in dct:
+        src = Source(dct['url'], dct['home'], dct['guest'], dct['homeQuote'], dct['guestQuote'])
+        print(src)
 
 
 class Source:
@@ -20,7 +21,7 @@ class Source:
         self.guestQuote = guestQuote
 
     def __str__(self):
-        return "source success"
+        return "url : %s , home : %s , guest : %s , homeQuote : %s , guestQuote : %s" % (self.url, self.home, self.guest, self.homeQuote, self.guestQuote)
 
 
 get_sources()
