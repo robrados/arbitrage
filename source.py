be44199b52e3fae308f4dc56fc119fe37483f61f
robra <robrados@gmail.com>
2018-06-15 13:05:54 +0200

    some changes

--- source.py
+++ source.py
@@ -1,22 +1,26 @@
 import json
 
-
 def get_sources():
-    json.loads(Source.file, object_hook=__as_sources)
+    with open('./configs/egb_config.json', encoding='utf-8') as file:
+        data = json.loads(file.read(), object_hook=__as_sources)
 
 
-def __as_sources(obj):
-    if '__source__' not in obj:
-        return obj
-    return Source(obj['url'], obj['home'], obj['home'], obj['home'], obj['home'])
+def __as_sources(dict):
+    list = []
+    for s in dict:
+        list.append(Source(s['url'], s['home'], s['guest'], s['homeQuote'], s['guestQuote']))
 
 
 class Source:
-    file = ''
-
     def __init__(self, url, home, guest, homeQuote, guestQuote):
         self.url = url
         self.home = home
         self.guest = guest
         self.homeQuote = homeQuote
         self.guestQuote = guestQuote
+
+    def __str__(self):
+        return "source success"
+
+
+get_sources()
